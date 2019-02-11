62ce12cef87f4cc8e59e7f7dca689e8cde88b005
afiorito <fioritoanthony@outlook.com>
2019-02-11 14:56:52 -0500

    version 0.1.0 – initial deploy

--- src/pages/notes.js
+++ src/pages/notes.js
@@ -3,9 +3,12 @@ import React from "react"
 import NotesNav from '../components/NotesNav';
 import SEO from "../components/seo"
 
+import '../components/layout.scss';
+import '../components/responsive.scss';
+
 const NotesPage = () => (
   <React.Fragment>
-    <SEO title="Page two" />
+    <SEO title="Notes" />
     <NotesNav />
     <h1>Notes</h1>
   </React.Fragment>
