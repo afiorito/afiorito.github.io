caf58bf7b307366d28c681ec9dd5b7b8f5c93c10
afiorito <fioritoanthony@outlook.com>
2019-02-17 09:48:17 -0500

    Added home link from notes pages

--- src/components/NotesNav.js
+++ src/components/NotesNav.js
@@ -1,18 +1,23 @@
 import React from "react"
+import { Link } from 'gatsby';
 
 import './NotesNav.scss';
 import ipadpro from '../images/ipadpro.svg';
 import search from '../images/search.svg';
+import home from '../images/home.svg';
 
 const NotesNav = ({ didUpdateSearch }) => (
   <nav className="NotesNav">
     <div className="container nav-group">
       <img className="brand-logo" src={ipadpro} alt="IPad Pro with Apple Pencil" />
       <h3 className="brand">Anthony's Notes</h3>
-      <form className="note-search__input">
-        <img className="note-search__icon" src={search} alt="Search Icon" />
-        <input onChange={didUpdateSearch} className="note-search__text-input" placeholder="Find a note..." />
-      </form>
+      <div className="nav-left">
+        <form className="note-search__input">
+          <img className="note-search__icon" src={search} alt="Search Icon" />
+          <input onChange={didUpdateSearch} className="note-search__text-input" placeholder="Find a note..." />
+        </form>
+        <Link to="/"><img className="nav-home__img" src={home} alt="Go to Home" /></Link>
+      </div>
     </div>
   </nav>
 )
