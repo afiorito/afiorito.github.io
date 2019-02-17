07a389a6f184d56a9fffb0cdcbf82ad82c699d74
afiorito <fioritoanthony@outlook.com>
2019-02-16 21:23:17 -0500

    Added document rotation and scaling

--- src/components/Document.js
+++ src/components/Document.js
@@ -3,15 +3,23 @@ import PropTypes from "prop-types"
 
 import noteImage from '../images/note.svg'
 import packageImage from '../images/package.svg'
+import { randomInt } from '../helpers/math';
 
 import './Document.scss'
 
 const Document = ({ note }) => {
-  const imgSrc = note.type === 'note' ? noteImage : packageImage;
+  const imgSrc = note.type === 'note' ? noteImage : packageImage
+  const sign = randomInt(1) === 0 ? 1 : -1;
+  const rotation = randomInt(8) * 0.5 * sign
 
   return (
     <div className="Document">
-      <img className="document__img" src={imgSrc} alt="Document" />
+      <img 
+        className="document__img" 
+        src={imgSrc} 
+        alt="Document"
+        style={{ transform: `rotate(${rotation}deg)` }}
+      />
       <div className="document-code__div">{note.code}</div>
       <div className="document-prof__div">{note.professor}</div>
       <div className="document-sem__div">{note.semester}</div>
---
+++ src/helpers/math.js
@@ -0,0 +1,3 @@
+export const randomInt = (maxInt) => {
+    return Math.floor(Math.random() * Math.floor(maxInt))
+}
